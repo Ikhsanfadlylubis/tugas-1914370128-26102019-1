e2327622dd2f9645a2ee421905049e0bf8dec649
Ikhsanfadlylubis <55613953+Ikhsanfadlylubis@users.noreply.github.com>
2019-11-01 13:49:38 +0700

    Add files via upload

---
+++ Program_1914370128_1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int NA,cek;
+    NA =74.2;
+    if (NA){
+        printf ("KETENTUAN.");
+    if (NA >= 70 ){
+        printf("LULUS.");
+    }else{
+    if (NA <= 70){
+        printf("TIDAK LULUS.");
+    }
+    return 0;
+ }
+}
+}
+
